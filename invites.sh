15c02bd6cfb2c34bccc38570d9e6b051c199e108
David Denton <denton.david@gmail.com>
2017-11-05 22:46:31 +0000

    files

---
+++ invites.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+declare -a arr=("name1" "name2" "name3")
+
+for i in "${arr[@]}"
+do
+   name=`echo "$i" | base64`
+   mkdir $name
+   cp index.html $name/index.html
+   git add .
+done
